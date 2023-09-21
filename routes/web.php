6a4108053b4280dd6d56b1905f75261bc0625ac2
ilsur <ilsur.vafin.00@yandex.ru>
2023-09-21 22:38:16 +0300

    finished 002-basics

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TodoController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::get('/todo', [TodoController::class, 'index'])->name('todo.index');
+Route::get('/todo/create', [TodoController::class, 'create'])->name('todo.create');
+Route::get('/todo/{id}', [TodoController::class, 'show'])->name('todo.show');
+Route::post('/todo', [TodoController::class, 'store'])->name('todo.store');
